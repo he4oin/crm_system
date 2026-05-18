41a241a066362d18e46d3b3eb52b4512a6ac7a07
he4oin <matveytr52@gmail.com>
2026-05-18 16:40:54 +0300

    Добавил функцию создания заказов с учетом карт лояльности

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def register_client(name,phone):
         "статус": "активен"
     }
 print(register_client(name = "Матвей" , phone = "+7904948892"))
+
+def create_order(item, price, loyalty_card=False):
+    if loyalty_card:
+        final_price = price * 0.9
+        print(f"Заказ {item} оформлен со скидкой. К оплате {final_price}")
+    else:
+        print(f"Заказ {item} оформлен. К оплате {price}")
+
+create_order(item="Ноутбук", price=30000, loyalty_card=True)
+
+
+    
